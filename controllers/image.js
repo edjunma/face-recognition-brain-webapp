32bffea0a26bf944ae2dac6ef60f92b9e107dcb6
edjunma <junwenma@buffalo.edu>
2019-02-19 00:13:28 -0500

    Fixed security concerns with API keys
    Cleaned up server.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+// You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '147046a299a541b4902c4001df6535a2'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+}
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
@@ -12,4 +27,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage
+  handleApiCall: handleApiCall
 }
